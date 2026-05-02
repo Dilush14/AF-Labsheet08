815502849e30ddecf801e2b35c6867f526a3d15a
dilush14 <diludilush963@gmail.com>
2026-05-03 00:04:33 +0530

    Complete exercise 03 Banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+function Banner() {
+  return (
+    <div style={{
+      background: "#2DD4BF",
+      padding: "20px",
+      textAlign: "center",
+      color: "white"
+    }}>
+      <h2>Special Offer!</h2>
+      <p>Get 50% discount today</p>
+    </div>
+  )
+}
+
+export default Banner
