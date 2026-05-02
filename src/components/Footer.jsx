a5f60c8e8c01e06708176e99664564652deeb93b
dilush14 <diludilush963@gmail.com>
2026-05-03 00:01:25 +0530

    Complete exercise 03 Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer style={{ marginTop: "20px", textAlign: "center" }}>
+      <p>© 2026 My React App</p>
+      <p>Follow us: Facebook | Instagram | LinkedIn</p>
+    </footer>
+  )
+}
+
+export default Footer
